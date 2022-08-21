2700944a526a8f4c3ead9652a4c3d5b69c0aa438
nicknamejv <jonvercabral@yahoo.com>
2022-08-20 20:44:52 -0700

    feat: created methods for saving, deleting, adding, and changing the note page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <PageView />
+  <PageList
+    @change-page="changePage"
+    @add-page="addPage"
+    :pages="pages"
+    :currentPage="index"
+  />
+  <PageView
+    @save-page="savePage"
+    @delete-page="deletePage"
+    :page="pages[index]"
+  />
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import PageView from './components/PageView.vue';
+  import PageList from './components/PageList.vue';
 
   export default defineComponent({
     name: 'App',
     components: {
       PageView,
+      PageList,
+    },
+    data() {
+      return {
+        pages: [],
+        index: 0,
+      };
+    },
+    methods: {
+      // Add a new page to the list of pages.
+      addPage() {
+        this.pages.push({
+          title: '',
+          content: '',
+        });
+        this.index = this.pages.length - 1;
+      },
+      // Change the current page.
+      changePage(index: any) {
+        this.index = index;
+      },
+      // TODO: Save the current page to the list of pages to localStorage later.
+      savePage() {
+        console.log('save page');
+      },
+      // Delete the current page from the list of pages.
+      deletePage() {
+        this.pages.splice(this.index, 1);
+        this.index = 0;
+      },
     },
   });
 </script>
